106e17e9dc0c1684726c7faa4c7958695e935ef5
ljubobratovicrelja <ljubobratovic.relja@gmail.com>
2015-08-05 22:58:02 +0200

    added linear algebra methods

--- include/linalg.hpp
+++ include/linalg.hpp
@@ -27,7 +27,143 @@
 // Relja Ljubobratovic, ljubobratovic.relja@gmail.com
 
 
-#pragma once
+#ifndef LINALG_HPP_YW7IEDT1
+#define LINALG_HPP_YW7IEDT1
+
 
 #include "fwd.hpp"
+#include "matrix.hpp"
+#include "matfunc.hpp"
 
+
+namespace cv {
+/*!
+ * @brief Compute the inverse of a matrix using the LU	factorization.
+ *
+ * This method inverts U and then computes inv(A) by solving the system
+ * inv(A)*L = inv(U) for inv(A).
+ */
+void CV_EXPORT invert(matrixr &matrix);
+
+/*!
+ * @brief Find rank of the matrix.
+ */
+size_t CV_EXPORT rank(const matrixr &matrix);
+
+/*!
+ * @brief Compute an LU factorization of a general M-by-N matrix A using partial pivoting with	row interchanges.
+ *
+ * Computes an LU	factorization of a general M-by-N matrix
+ * A using partial pivoting	with row interchanges. The factorization
+ * has the form :
+ * @code
+ A = P * L * U
+ @endcode
+ * where P is a permutation matrix, L is lower triangular with
+ * unit diagonal elements (lower trapezoidal if m > n), and U
+ * is upper	triangular (upper trapezoidal if m < n).
+ */
+void CV_EXPORT lu_decomp(const matrixr &A, matrixr &L, matrixr &U,
+		matrixr &P);
+
+/*!
+ * @brief Compute singular	value decomposition (SVD) of a real M-by-N
+ * matrix A.
+ *
+ * @returns Factorized matrix A.
+ */
+matrixr CV_EXPORT svd_decomp(const matrixr &A, matrixr &U, matrixr &S,
+		matrixr &VT);
+
+/*!
+ * @brief Solve a system of linear equations using LU decomposition.
+ *
+ * Solve a system of linear equations  A * X = B or A' * X = B
+ * with a general N-by-N matrix	A using	the LU factorization.
+ * A * X = B or A' * X = B with a general N-by-N matrix
+ * A using	the LU factorization.
+ *
+ * Uses lapack dgesv/sgesv
+ *
+ * \throws std::runtime_error
+ */
+void CV_EXPORT lu_solve(const matrixr &A, const matrixr &B, matrixr &X);
+
+/*!
+ * @brief Solve null space equation of a matrix.
+ *
+ * @param in Input matrix of arbitrary size.
+ * @param nullSpace Null-space of the input matrix.
+ */
+void CV_EXPORT null_solve(const matrixr &in, matrixr &nullSpace);
+
+/*!
+ * @brief Perform general matrix multiplication.
+ *
+ * Wraps GEMM routine from BLAS: C = alpha*A*B + beta*C
+ *
+ * @param A Matrix of size mxk
+ * @param B Matrix of size kxn
+ * @param C If beta is 0, null matrix can be passed, in opposite, needs to be mxn. On exit is mxn.
+ * @param alpha Scalar multiplication for matrix A.
+ * @param beta Scalar multiplication for matrix C - pass 0 for operation C = AxB
+ */
+void CV_EXPORT gemm(const matrixr &A, const matrixr &B, matrixr &C,
+		real_t alpha = 1.0, real_t beta = 0.0);
+
+/*!
+ * @brief Calculate eigenvalues of a square matrix.
+ *
+ * @param in Input matrix.
+ * @param wr Vector containing real parts of eigenvalues.
+ * @param wi Vector containing imaginary parts of eigenvalues.
+ *
+ * \throws std::runtime_error if input matrix is not a square matrix.
+ */
+void CV_EXPORT eigenvalues(const matrixr &in, vector<real_t> &wr, vector<real_t> &wi);
+
+/*!
+ * @brief Calculate eigenvectors of a square matrix.
+ *
+ * @param in Input matrix.
+ * @param lv Vector containing left eigenvectors.
+ * @param rv Vector containing right eigenvectors.
+ *
+ * \throws std::runtime_error if input matrix is not a square matrix.
+ */
+void CV_EXPORT eigenvectors(const matrixr &in, vector<real_t> &lv, vector<real_t> &rv);
+
+/*!
+ * @brief Calculate eigenvalues and eigenvectors of a square matrix. Wraps GEEV routine from Lapack.
+ *
+ * @param in Input matrix.
+ * @param wr Vector containing real parts of eigenvalues.
+ * @param wi Vector containing imaginary parts of eigenvalues.
+ * @param lv Vector containing left eigenvectors.
+ * @param rv Vector containing right eigenvectors.
+ *
+ * \throws std::runtime_error if input matrix is not a square matrix.
+ */
+void CV_EXPORT geev(const matrixr &in, vector<real_t> &wr, vector<real_t> &wi, matrixr &lv, matrixr &rv);
+
+/*!
+ * Evaluate Rodrigues rotation formula to convert rotation vector to matrix and vice-versa.
+ *
+ * @param src Input matrix, should be 3x1, 1x3 as vector or 3x3 as matrix.
+ * @param dst Output matrix, if input is a vector output is a matrix and vice-versa.
+ * @param jacobian If input, returns Jacobian matrix, which is a matrix of partial
+ * derivatives of the output array components with respect to the input array components.
+ *
+ * @warning
+ * Disclamer - implementation borrowed from opencv/calib3d module. (cvRodrigues2)
+ * 
+ * \note Jacobian matrix needs to be initialized to 9x3 or 3x9 if output is expected. If jacobian is
+ * initialized differently, mta::Exception will be thrown.
+ *
+ * \throws std::runtime_error.
+ *
+ */
+int CV_EXPORT rodrigues_solve(const matrixr &src, matrixr &dst, matrixr *jacobian);
+
+}
+#endif /* end of include guard: LINALG_HPP_YW7IEDT1 */
