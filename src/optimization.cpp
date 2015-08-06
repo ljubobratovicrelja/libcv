b3064491001f9d93a4230e31fff9c65a74eac8ea
ljubobratovicrelja <ljubobratovic.relja@gmail.com>
2015-08-06 23:25:10 +0200

    added lmdif1 wrapper

---
+++ src/optimization.cpp
@@ -0,0 +1,47 @@
+// The MIT License (MIT)
+// 
+// Copyright (c) 2015 Relja Ljubobratovic, ljubobratovic.relja@gmail.com
+// 
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+// 
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+// 
+// Author:
+// Relja Ljubobratovic, ljubobratovic.relja@gmail.com
+
+#include "../include/optimization.hpp"
+
+#include <limits>
+
+
+namespace cv {
+
+int lmdif1(optimization_fcn fcn, int m, int n, double *x, double *fvec, double tol) {
+	ASSERT(m > n && x && fvec && tol > std::numeric_limits<double>::min());
+
+	int info = 0;
+
+	auto *iwa = new int[n];
+	auto lwa = (m*n)+(5*n)+m+10;
+	auto *wa = new double[lwa];
+
+	lmdif1_(fcn, &m, &n, x, fvec, &tol, &info, iwa, wa, &lwa);
+
+	return info;
+}
+
+}
