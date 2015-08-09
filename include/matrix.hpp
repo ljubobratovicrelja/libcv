48d3991caa63f1bf7c47632f1d91d84778b8abb3
ljubobratovicrelja <ljubobratovic.relja@gmail.com>
2015-08-09 13:37:35 +0200

    added vector-matrix cross operators and vice-versa

--- include/matrix.hpp
+++ include/matrix.hpp
@@ -782,6 +782,53 @@ matrix<_Tp> operator*(const matrix<_Tp>& lhs, const matrix<_Tp>& rhs) {
 	return retVal;
 }
 
+template<typename _Tp>
+vector<_Tp> operator*(const matrix<_Tp>& lhs, const vector<_Tp>& rhs) {
+	ASSERT(lhs.rows() == rhs.length() || lhs.cols() == rhs.length());
+	bool transposed = (lhs.rows() == rhs.length()) ? true : false;
+	
+	cv::matrix<_Tp> res;
+	cv::matrix<_Tp> rhs_m(rhs, transposed);
+	res = lhs * rhs_m;
+
+	return vector<_Tp>(res.data(),res.data_begin(), res.shape().product(), 1, res.refcounter());
+}
+
+template<typename _Tp>
+vectorx<_Tp, 3> operator*(const matrix<_Tp>& lhs, const vectorx<_Tp, 3>& rhs) {
+	ASSERT(lhs.rows() == 3 || lhs.cols() == 3);
+	bool transposed = (lhs.rows() == 3) ? true : false;
+	
+	cv::matrix<_Tp> res;
+	cv::matrix<_Tp> rhs_m(rhs, transposed);
+	res = lhs * rhs_m;
+
+	return vectorx<_Tp, 3>(res.data()[0], res.data()[1], res.data()[2]);
+}
+
+template<typename _Tp>
+vector<_Tp> operator*(const vector<_Tp>& lhs, const matrix<_Tp>& rhs) {
+	ASSERT(rhs.rows() == lhs.length() || rhs.cols() == lhs.length());
+	bool transposed = (rhs.cols() == lhs.length()) ? true : false;
+	
+	cv::matrix<_Tp> res;
+	cv::matrix<_Tp> rhs_m(lhs, transposed);
+	res = lhs * rhs_m;
+
+	return vector<_Tp>(res.data(),res.data_begin(), res.shape().product(), 1, res.refcounter());
+}
+
+template<typename _Tp>
+vectorx<_Tp, 3> operator*(const vectorx<_Tp, 3>& lhs, const matrix<_Tp>& rhs) {
+	ASSERT(lhs.rows() == 3 || lhs.cols() == 3);
+	bool transposed = (lhs.cols() == 3) ? true : false;
+	
+	cv::matrix<_Tp> res;
+	cv::matrix<_Tp> rhs_m(rhs, transposed);
+	res = lhs * rhs_m;
+
+	return vectorx<_Tp, 3>(res.data()[0], res.data()[1], res.data()[2]);
+}
 
 //! Addition operator.
 template<typename _Tp>
