facf4e86aec4784f81f53e8d3d35e3997bd04253
ljubobratovicrelja <ljubobratovic.relja@gmail.com>
2015-08-22 14:15:25 +0200

    added real_t adaptation for cminpack

--- include/cminpack.h
+++ include/cminpack.h
@@ -1,36 +1,42 @@
 #ifndef __CMINPACK_H
 #define __CMINPACK_H
 
+#ifdef CV_REAL_TYPE_DOUBLE
+typedef double real_t;
+#else
+typedef float real_t;
+#endif
+
 #if	defined(__cplusplus)
 extern "C" {
 #endif
 
 
-double enorm(int n, double x[]);
-double rownorm(int m, int r, int c, double **x);
-double colnorm(int m, int r, int c, double **x);
+real_t enorm(int n, real_t x[]);
+real_t rownorm(int m, int r, int c, real_t **x);
+real_t colnorm(int m, int r, int c, real_t **x);
 
-void fdjac2(void f(int,int, double*,double*,int*),int m,int n,double x[],double fvec[],double **fjac,
-            int *iflag,double epsfcn,double wa[]);
+void fdjac2(void f(int,int, real_t*,real_t*,int*),int m,int n,real_t x[],real_t fvec[],real_t **fjac,
+            int *iflag,real_t epsfcn,real_t wa[]);
 
-void lmpar(int n,double **fjac,int ipvt[],double diag[],
-           double qtf[],double delta,double *par,double wa1[],
-           double wa2[],double wa3[],double wa4[]);
+void lmpar(int n,real_t **fjac,int ipvt[],real_t diag[],
+           real_t qtf[],real_t delta,real_t *par,real_t wa1[],
+           real_t wa2[],real_t wa3[],real_t wa4[]);
 
-void qrfac(int m,int n,double **a,int pivot,int ipvt[],
-           double rdiag[],double acnorm[],double wa[]);
+void qrfac(int m,int n,real_t **a,int pivot,int ipvt[],
+           real_t rdiag[],real_t acnorm[],real_t wa[]);
 
-void lmdif(void f(int,int, double*,double*,int*),int m,int n,double x[],int msk[],double fvec[],
-           double ftol,double xtol,double gtol,int maxfev,double epsfcn,
-           double diag[],int mode,double factor,int *info,int *nfev,
-           double **fjac,int ipvt[],double qtf[],double wa1[],double wa2[],
-           double wa3[],double wa4[]);
+void lmdif(void f(int,int, real_t*,real_t*,int*),int m,int n,real_t x[],int msk[],real_t fvec[],
+           real_t ftol,real_t xtol,real_t gtol,int maxfev,real_t epsfcn,
+           real_t diag[],int mode,real_t factor,int *info,int *nfev,
+           real_t **fjac,int ipvt[],real_t qtf[],real_t wa1[],real_t wa2[],
+           real_t wa3[],real_t wa4[]);
 
-void qrsolv(int n,double **r,int ipvt[],double diag[],
-            double qtb[],double x[],double sdiag[],double wa[]);
+void qrsolv(int n,real_t **r,int ipvt[],real_t diag[],
+            real_t qtb[],real_t x[],real_t sdiag[],real_t wa[]);
 
-int lmdif0(void f(int,int, double*,double*,int*),int m,int n,double x[],int msk[],double fvec[],
-           double tol,int *info,int *nfev);
+int lmdif0(void f(int,int, real_t*,real_t*,int*),int m,int n,real_t x[],int msk[],real_t fvec[],
+           real_t tol,int *info,int *nfev);
 
 
 #if defined(__cplusplus)
